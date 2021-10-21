bba0ffd14fd796fd4c9502b02a0236e0f5733d77
Manjiri Birajdar <15740697+ManjiriBirajdar@users.noreply.github.com>
2021-10-21 21:53:53 +0200

    updated code for pin numbers of buttons
    updated code for pin numbers of buttons
    23 pin - preview
    21 pin - capture

--- Camera-Capture/Camera_Picture_Preview.py
+++ Camera-Capture/Camera_Picture_Preview.py
@@ -6,17 +6,16 @@ from picamera import PiCamera
 from datetime import datetime
 from signal import pause
  
-preview_button = Button(2)
-capture_button = Button(3)
+preview_button = Button(23)
+capture_button = Button(21)
 image_Capture = PiCamera()
-resol_X = 1024
-resol_Y = 768
+resol_X = 2560
+resol_Y = 1920
  
 def store_Image():
     timestamp = datetime.now().isoformat()
-	image_Capture.resolution = (resol_X, resol_Y)
-	
-    image_Capture.capture('/home/pi/%s.jpg' % timestamp)
+    image_Capture.resolution = (resol_X, resol_Y)
+    image_Capture.capture('/home/pi/Documents/code/dataset/%s.jpg' % timestamp)
  
 preview_button.when_pressed = image_Capture.start_preview
 preview_button.when_released = image_Capture.stop_preview
