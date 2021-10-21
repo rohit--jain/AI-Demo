29880fabeff45ff0d5d1fc6b64a695919673c7a5
Rohit Jain <rohit.jain.324@gmail.com>
2021-10-22 00:01:21 +0200

    Looping in camera action added

--- Camera-Capture/Camera_Picture_Preview.py
+++ Camera-Capture/Camera_Picture_Preview.py
@@ -11,14 +11,23 @@ capture_button = Button(21)
 image_Capture = PiCamera()
 resol_X = 2560
 resol_Y = 1920
+enable_Run = True
+no_Pics = 0
  
 def store_Image():
     timestamp = datetime.now().isoformat()
     image_Capture.resolution = (resol_X, resol_Y)
     image_Capture.capture('/home/pi/Documents/code/dataset/%s.jpg' % timestamp)
+    no_Pics += 1
  
-preview_button.when_pressed = image_Capture.start_preview
-preview_button.when_released = image_Capture.stop_preview
-capture_button.when_pressed = store_Image
- 
+while(enable_Run):
+    print("Press Preview / Capture Button for Action...")
+    preview_button.when_pressed = image_Capture.start_preview
+    print("Camera Preview Started")
+    preview_button.when_released = image_Capture.stop_preview
+    print("Camera Preview Stopped")
+    capture_button.when_pressed = store_Image
+    print("Camera Shot: " + str(no_Pics) + " taken!")
+
+print("Total Number of Pictures Clicked: "+ str(no_Pics))
 pause()
